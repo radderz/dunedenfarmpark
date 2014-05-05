2e97250f86d3130824609ba84afec04c2a1699fa
markr <mark.radcliffe@blackhawktracking.com>
2014-05-05 23:25:43 +1200

    Added Contact Form

--- DunedenFarmPark/DunedenFarmPark/Controllers/HomeController.cs
+++ DunedenFarmPark/DunedenFarmPark/Controllers/HomeController.cs
@@ -36,7 +36,15 @@ namespace DunedenFarmPark.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "We will reply as quickly as possible to your query." + Environment.NewLine + "If emailing please include a landline contact number and time when you would be available for us to call you to discuss your requirements.";
+
+            ViewBag.Phone = "03 476 7100";
+
+            ViewBag.Mobile = "021 02972411";
+
+            ViewBag.Email = "sue@dunedenfarmpark.co.nz";
+
+            ViewBag.Address = "Dun Eden Farm Park,17 Ettrick Street,Glenross,Dunedin 9011";
 
             return View();
         }
